f62d031832b87568c779bce30fecd8b20a2d77ac
Oleksii Khovan <okhovan@lohika.com>
2022-08-03 13:50:28 +0300

    add 4D testcase

--- src/plugins/intel_gpu/tests/test_cases/scatter_elements_update_gpu_test.cpp
+++ src/plugins/intel_gpu/tests/test_cases/scatter_elements_update_gpu_test.cpp
@@ -235,6 +235,28 @@ std::vector<SEUParams<T>> generateSEUParams3D() {
 }
 
 template<typename T>
+std::vector<SEUParams<T>> generateSEUParams4D() {
+    const std::vector<SEUParams<T>> result = {
+        { 5,
+          // data
+          tensor{2, 4, 2, 1, 1, 3},
+          getValues<T>({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                        24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47}),
+          // indices
+          tensor{2, 1, 1, 1, 1, 2},
+          getValues<T>({2, 1, 1, 1, 2}),
+          // updates
+          getValues<T>({-100, -110, -120, -130}),
+          // expected
+          getValues<T>({0, 1, -100, -110, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                        24, -120, 26, -130, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47}),
+        }
+    };
+
+    return result;
+}
+
+template<typename T>
 float getError();
 
 template<>
@@ -418,5 +440,14 @@ INSTANTIATE_TEST_SUITE_P(scatter_elements_update_gpu_formats_test_i32_3d,
                                  ::testing::ValuesIn(generateSEUParams3D<int32_t>()),
                                  ::testing::Values(format::bfzyx),
                                  ::testing::ValuesIn(formats3D)
+                         ),
+                         PrintToStringParamName());
+
+INSTANTIATE_TEST_SUITE_P(scatter_elements_update_gpu_formats_test_f32_4d,
+                         scatter_elements_update_gpu_formats_test_f32,
+                         ::testing::Combine(
+                                 ::testing::ValuesIn(generateSEUParams4D<float>()),
+                                 ::testing::Values(format::bfwzyx),
+                                 ::testing::Values(format::bfwzyx)
                          ),
                          PrintToStringParamName());
