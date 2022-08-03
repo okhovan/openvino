9d60ab51b59f49a9ff2b1944d8ebb4b25783dcdd
Oleksii Khovan <okhovan@lohika.com>
2022-08-03 11:43:49 +0300

    add parameterized test

--- src/plugins/intel_gpu/tests/test_cases/scatter_elements_update_gpu_test.cpp
+++ src/plugins/intel_gpu/tests/test_cases/scatter_elements_update_gpu_test.cpp
@@ -16,7 +16,7 @@ using namespace cldnn;
 using namespace ::tests;
 
 
-const std::vector<format::type> formats2D{
+const std::vector<format::type> z_formats2D{
         format::bfyx,
         format::b_fs_yx_fsv16,
         format::b_fs_yx_fsv32,
@@ -53,7 +53,7 @@ TEST(scatter_elements_update_gpu_fp16, d2411_axisF) {
     const auto data_type = data_types::f16;
     const auto plain_format = format::bfyx;
 
-    for(const auto target_format : formats2D) {
+    for(const auto target_format : z_formats2D) {
 
         auto input1 = engine.allocate_memory({data_types::f16, format::bfyx, tensor{2, 4, 1, 1}}); // Dictionary
         auto input2 = engine.allocate_memory({data_types::f16, format::bfyx, tensor{2, 2, 1, 1}}); // Indexes
@@ -111,3 +111,234 @@ TEST(scatter_elements_update_gpu_fp16, d2411_axisF) {
         }
     }
 }
+
+
+
+namespace {
+template<typename T>
+struct SEUParams {
+    int64_t axis;
+    tensor data_tensor;
+    std::vector<T> data;
+    tensor indices_tensor;
+    std::vector<T> indices;
+    std::vector<T> updates;
+    std::vector<T> expected;
+};
+
+template<typename T>
+using SEUParamsWithFormat = std::tuple<
+    SEUParams<T>,
+    format::type,     // source (plain) layout
+    format::type      // target (blocked) layout
+>;
+
+const std::vector<format::type> formats2D{
+        format::bfyx,
+        format::b_fs_yx_fsv16,
+        format::b_fs_yx_fsv32,
+        format::bs_fs_yx_bsv16_fsv16,
+        format::bs_fs_yx_bsv32_fsv16,
+        format::bs_fs_yx_bsv32_fsv32
+};
+
+const std::vector<format::type> formats3D{
+        format::bfzyx,
+        format::b_fs_zyx_fsv16,
+        format::bs_fs_zyx_bsv16_fsv16
+};
+
+const std::vector<format::type> formats4D{
+        format::bfwzyx
+};
+
+template<typename T>
+std::vector<T> getValues(const std::vector<float> &values) {
+    std::vector<T> result(values.begin(), values.end());
+    return result;
+}
+
+template<typename T>
+std::vector<SEUParams<T>> generateSEUParams2D() {
+    const std::vector<SEUParams<T>> result = {
+        {   1,
+            // data
+            tensor(2, 4, 1, 1),
+            getValues<T>({
+                3, 6, 5, 4,
+                1, 7, 2, 9}),
+            // indices
+            tensor(2, 2, 1, 1),
+            getValues<T>({
+                0, 1,
+                2, 3}),
+            // updates
+            getValues<T>({
+                10, 11,
+                12, 13}),
+            // expected
+            getValues<T>({
+                10, 11, 5, 4,
+                1, 7, 12, 13})
+        },
+    };
+
+    return result;
+}
+
+template<typename T>
+float getError();
+
+template<>
+float getError<float>() {
+    return 0.001;
+}
+
+template<>
+float getError<half_t>() {
+    return 0.2;
+}
+
+template<>
+float getError<int32_t>() {
+    return 0.0;
+}
+
+std::string toString(const format::type format) {
+    switch(format) {
+        case format::bfyx:
+            return "bfyx";
+        case format::b_fs_yx_fsv16:
+            return "b_fs_yx_fsv16";
+        case format::b_fs_yx_fsv32:
+            return "b_fs_yx_fsv32";
+        case format::bs_fs_yx_bsv16_fsv16:
+            return "bs_fs_yx_bsv16_fsv16";
+        case format::bs_fs_yx_bsv32_fsv16:
+            return "bs_fs_yx_bsv32_fsv16";
+        case format::bs_fs_yx_bsv32_fsv32:
+            return "bs_fs_yx_bsv32_fsv32";
+        case format::bfzyx:
+            return "bfzyx";
+        case format::b_fs_zyx_fsv16:
+            return "b_fs_zyx_fsv16";
+        case format::bs_fs_zyx_bsv16_fsv16:
+            return "bs_fs_zyx_bsv16_fsv16";
+        default:
+            return std::to_string(format);
+    }
+}
+
+struct PrintToStringParamName {
+    template<class T>
+    std::string operator()(const testing::TestParamInfo<SEUParamsWithFormat<T> > &param) {
+        std::stringstream buf;
+        SEUParams<T> p;
+        format::type plain_format;
+        format::type target_format;
+        std::tie(p, plain_format, target_format) = param.param;
+        buf << "_axis=" << p.axis
+            << "_data=" << p.data_tensor.to_string()
+            << "_indices=" << p.indices_tensor.to_string()
+            << "_plainFormat=" << toString(plain_format)
+            << "_targetFormat=" << toString(target_format);
+        return buf.str();
+    }
+};
+}; // namespace
+
+template<typename T>
+struct scatter_elements_update_gpu_formats_test
+        : public ::testing::TestWithParam<SEUParamsWithFormat<T> > {
+public:
+    void test() {
+        const auto data_type = type_to_data_type<T>::value;
+        SEUParams<T> params;
+        format::type plain_format;
+        format::type target_format;
+
+        std::tie(params, plain_format, target_format) = this->GetParam();
+
+        auto& engine = get_test_engine();
+        const auto data = engine.allocate_memory({data_type, plain_format, params.data_tensor});
+        const auto indices = engine.allocate_memory({data_type, plain_format, params.indices_tensor});
+        const auto updates = engine.allocate_memory({data_type, plain_format, params.indices_tensor});
+
+        set_values(data, params.data);
+        set_values(indices, params.indices);
+        set_values(updates, params.updates);
+
+        topology topology;
+        topology.add(input_layout("InputData", data->get_layout()));
+        topology.add(input_layout("InputIndices", indices->get_layout()));
+        topology.add(input_layout("InputUpdates", updates->get_layout()));
+        topology.add(reorder("InputData_Reordered", "InputData", target_format, data_type));
+        topology.add(reorder("InputIndices_Reordered", "InputIndices", target_format, data_type));
+        topology.add(reorder("InputUpdates_Reordered", "InputUpdates", target_format, data_type));
+        topology.add(
+                scatter_elements_update("ScatterEelementsUpdate", "InputData_Reordered", "InputIndices_Reordered",
+                                        "InputUpdates_Reordered", params.axis)
+        );
+        topology.add(reorder("ScatterEelementsUpdate_Plain", "ScatterEelementsUpdate", plain_format, data_type));
+
+        network network{engine, topology};
+
+        network.set_input_data("InputData", data);
+        network.set_input_data("InputIndices", indices);
+        network.set_input_data("InputUpdates", updates);
+
+        const auto outputs = network.execute();
+        const auto output = outputs.at("ScatterEelementsUpdate_Plain").get_memory();
+        const cldnn::mem_lock<T> output_ptr(output, get_test_stream());
+
+        ASSERT_EQ(params.data.size(), output_ptr.size());
+        ASSERT_EQ(params.expected.size(), output_ptr.size());
+        for (uint32_t i = 0; i < output_ptr.size(); i++) {
+            EXPECT_NEAR(output_ptr[i], params.expected[i], getError<T>()) << "format=" << toString(target_format) << ", i=" << i;
+        }
+    }
+};
+
+using scatter_elements_update_gpu_formats_test_f32 = scatter_elements_update_gpu_formats_test<float>;
+using scatter_elements_update_gpu_formats_test_f16 = scatter_elements_update_gpu_formats_test<half_t>;
+using scatter_elements_update_gpu_formats_test_i32 = scatter_elements_update_gpu_formats_test<int32_t>;
+
+TEST_P(scatter_elements_update_gpu_formats_test_f32, basic) {
+    ASSERT_NO_FATAL_FAILURE(test());
+}
+
+TEST_P(scatter_elements_update_gpu_formats_test_f16, basic) {
+    ASSERT_NO_FATAL_FAILURE(test());
+}
+
+TEST_P(scatter_elements_update_gpu_formats_test_i32, basic) {
+    ASSERT_NO_FATAL_FAILURE(test());
+}
+
+
+INSTANTIATE_TEST_SUITE_P(scatter_elements_update_gpu_formats_test_f32_2d,
+                         scatter_elements_update_gpu_formats_test_f32,
+                         ::testing::Combine(
+                                 ::testing::ValuesIn(generateSEUParams2D<float>()),
+                                 ::testing::Values(format::bfyx),
+                                 ::testing::ValuesIn(formats2D)
+                                 ),
+                         PrintToStringParamName());
+
+INSTANTIATE_TEST_SUITE_P(scatter_elements_update_gpu_formats_test_f16_2d,
+                         scatter_elements_update_gpu_formats_test_f16,
+                         ::testing::Combine(
+                                 ::testing::ValuesIn(generateSEUParams2D<half_t>()),
+                                 ::testing::Values(format::bfyx),
+                                 ::testing::ValuesIn(formats2D)
+                                 ),
+                         PrintToStringParamName());
+
+INSTANTIATE_TEST_SUITE_P(scatter_elements_update_gpu_formats_test_i32_2d,
+                         scatter_elements_update_gpu_formats_test_i32,
+                         ::testing::Combine(
+                                 ::testing::ValuesIn(generateSEUParams2D<int32_t>()),
+                                 ::testing::Values(format::bfyx),
+                                 ::testing::ValuesIn(formats2D)
+                                 ),
+                         PrintToStringParamName());
