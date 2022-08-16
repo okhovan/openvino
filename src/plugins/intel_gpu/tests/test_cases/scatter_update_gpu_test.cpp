b720bf659ab5e3bdecb49b801a2aff3b42a5c930
Oleksii Khovan <okhovan@lohika.com>
2022-08-16 14:59:54 +0300

    blocked scatter_update_gpu_int32.d121251_bfwzyx_axisB fail due to too few arguments in INPUT2_GET_INDEX

--- src/plugins/intel_gpu/tests/test_cases/scatter_update_gpu_test.cpp
+++ src/plugins/intel_gpu/tests/test_cases/scatter_update_gpu_test.cpp
@@ -1222,62 +1222,69 @@ TEST(scatter_update_gpu_int32, d121251_bfwzyx_axisB) {
 
     auto& engine = get_test_engine();
 
-    auto input1 = engine.allocate_memory({ data_types::i32, format::bfwzyx, tensor{ batch(1), feature(2), spatial(1, 5, 2, 1) }}); // Dictionary
-    auto input2 = engine.allocate_memory({ data_types::i32, format::bfyx, tensor{ 2, 2, 1, 1 } });                                       // Indexes
-    auto input3 = engine.allocate_memory({ data_types::i32, format::bfwzyx, tensor{ batch(1), feature(2), spatial(2, 2, 2, 1) }}); // Updates
-    auto axis = 4;
+    for(const auto target_format : formats2D) {
+        auto input1 = engine.allocate_memory(
+                {data_types::i32, format::bfwzyx, tensor{batch(1), feature(2), spatial(1, 5, 2, 1)}}); // Dictionary
+        auto input2 = engine.allocate_memory({data_types::i32, plain_2d_format,
+                                              tensor{2, 2, 1, 1}});                                       // Indexes
+        auto input3 = engine.allocate_memory(
+                {data_types::i32, format::bfwzyx, tensor{batch(1), feature(2), spatial(2, 2, 2, 1)}}); // Updates
+        auto axis = 4;
 
-    set_values(input1, {
-        0, 1, 2, 3, 4,
-        5, 6, 7, 8, 9,
-        10, 11, 12, 13, 14,
-        15, 16, 17, 18, 19
-    });
+        set_values(input1, {
+                0, 1, 2, 3, 4,
+                5, 6, 7, 8, 9,
+                10, 11, 12, 13, 14,
+                15, 16, 17, 18, 19
+        });
 
-    set_values(input2, {
-        2, 1,
-        0, 4
-    });
+        set_values(input2, {
+                2, 1,
+                0, 4
+        });
 
-    set_values(input3, {
-        20, 30,
-        40, 50,
-        60, 70,
-        80, 90,
-        100,  110,
-        120,  130,
-        140,  150,
-        160,  170
-    });
+        set_values(input3, {
+                20, 30,
+                40, 50,
+                60, 70,
+                80, 90,
+                100, 110,
+                120, 130,
+                140, 150,
+                160, 170
+        });
 
-    topology topology;
-    topology.add(input_layout("InputDictionary", input1->get_layout()));
-    topology.add(input_layout("InputText", input2->get_layout()));
-    topology.add(input_layout("InputUpdates", input3->get_layout()));
-    topology.add(
-        scatter_update("scatter_update", "InputDictionary", "InputText", "InputUpdates", axis)
-    );
+        topology topology;
+        topology.add(input_layout("InputDictionary", input1->get_layout()));
+        topology.add(input_layout("InputText", input2->get_layout()));
+        topology.add(input_layout("InputUpdates", input3->get_layout()));
+        topology.add(reorder("TextReordered", "InputText", target_format, data_types::i32));
+        topology.add(
+                scatter_update("scatter_update", "InputDictionary", "TextReordered", "InputUpdates", axis)
+        );
 
-    network network(engine, topology);
+        network network(engine, topology);
 
-    network.set_input_data("InputDictionary", input1);
-    network.set_input_data("InputText", input2);
-    network.set_input_data("InputUpdates", input3);
+        network.set_input_data("InputDictionary", input1);
+        network.set_input_data("InputText", input2);
+        network.set_input_data("InputUpdates", input3);
 
-    auto outputs = network.execute();
+        auto outputs = network.execute();
 
-    auto output = outputs.at("scatter_update").get_memory();
-    cldnn::mem_lock<int> output_ptr(output, get_test_stream());
+        auto output = outputs.at("scatter_update").get_memory();
+        cldnn::mem_lock<int> output_ptr(output, get_test_stream());
 
-    std::vector<int> expected_results = {
-        40,  30,   20,  3, 50,
-        80,  70,   60,  8, 90,
-        120, 110, 100, 13, 130,
-        160, 150, 140, 18, 170
-    };
+        std::vector<int> expected_results = {
+                40, 30, 20, 3, 50,
+                80, 70, 60, 8, 90,
+                120, 110, 100, 13, 130,
+                160, 150, 140, 18, 170
+        };
 
-    for (size_t i = 0; i < expected_results.size(); ++i) {
-        EXPECT_EQ(expected_results[i], output_ptr[i]);
+        for (size_t i = 0; i < expected_results.size(); ++i) {
+            EXPECT_EQ(expected_results[i], output_ptr[i])
+                << "i=" << i << ", target_format=" << target_format;
+        }
     }
 }
 
