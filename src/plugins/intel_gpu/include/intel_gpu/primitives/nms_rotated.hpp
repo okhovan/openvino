205182fdad0c2c46348f2a20fcdf1550995c0e57
Oleksii Khovan <oleksii.khovan@capgemini.com>
2023-09-27 10:43:58 +0200

    primitive & CreateOp() - can be compiled

---
+++ src/plugins/intel_gpu/include/intel_gpu/primitives/nms_rotated.hpp
@@ -0,0 +1,104 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+#include "primitive.hpp"
+#include "intel_gpu/graph/serialization/string_serializer.hpp"
+
+#include <vector>
+
+namespace cldnn {
+
+/// @brief Performs non max suppression of rotated input boxes and returns indices of selected boxes.
+struct nms_rotated : public primitive_base<nms_rotated> {
+    CLDNN_DECLARE_PRIMITIVE(nms_rotated)
+
+    /// @brief Creates NMSRotated primitive.
+    /// @param id This primitive id.
+    /// @param boxes Id of input primitive with bounding boxes.
+    /// @param scores Id of input primitive with boxes scores per class.
+    /// @param max_output_boxes_per_class Id of input primitive specifying max number of boxes to be selected per class.
+    /// @param iou_threshold Id of input primitive specifying threshold value for IOU.
+    /// @param score_threshold Id of input primitive specifying minimum score for the box to be processed.
+    /// @param sort_result_descending Specifies whether it is necessary to sort selected boxes across batches or not.
+    /// @param output_data_type Output data type
+    /// @param clockwise Specifies whether it is necessary to sort selected boxes across batches or not.
+    /// @param selected_scores Id of primitive specifying output for scores for each selected box.
+    /// @param valid_outputs Id of primitive specifying output for total number of selected boxes.
+    nms_rotated(const primitive_id& id,
+                const std::vector<input_info> inputs,
+/*
+                const input_info& boxes,
+                const input_info& scores,
+                const input_info& max_output_boxes_per_class,
+                const input_info& iou_threshold,
+                const input_info& score_threshold,
+*/
+                const bool sort_result_descending = true,
+                const data_types output_data_type = data_types::i32,
+                bool clockwise = true,
+                const primitive_id selected_scores = primitive_id{},
+                const primitive_id valid_outputs = primitive_id{})
+        : primitive_base{id, inputs, {padding{}}, {optional_data_type{}} /*do we need num_outputs here???*/}
+        , sort_result_descending(sort_result_descending)
+        , output_data_type(output_data_type)
+        , clockwise(clockwise)
+        , selected_scores(selected_scores)
+        , valid_outputs(valid_outputs) {}
+
+    bool sort_result_descending;
+    data_types output_data_type;
+    bool clockwise;
+    primitive_id selected_scores;
+    primitive_id valid_outputs;
+
+    size_t hash() const override {
+        size_t seed = primitive::hash();
+        seed = hash_combine(seed, sort_result_descending);
+        seed = hash_combine(seed, output_data_type);
+        seed = hash_combine(seed, clockwise);
+        seed = hash_combine(seed, selected_scores.empty());
+        seed = hash_combine(seed, valid_outputs.empty());
+        return seed;
+    }
+
+    bool operator==(const primitive& rhs) const override {
+        if (!compare_common_params(rhs))
+            return false;
+
+        const auto rhs_casted = downcast<const nms_rotated>(rhs);
+
+        #define cmp_fields(name) name == rhs_casted.name
+        return cmp_fields(sort_result_descending) &&
+               cmp_fields(output_data_type) &&
+               cmp_fields(clockwise);
+        #undef cmp_fields
+    }
+
+    std::vector<std::reference_wrapper<const primitive_id>> get_dependencies() const override {
+        std::vector<std::reference_wrapper<const primitive_id>> ret;
+        ret.emplace_back(selected_scores);
+        ret.emplace_back(valid_outputs);
+        return ret;
+    }
+
+    void save(BinaryOutputBuffer& ob) const override {
+        primitive_base<nms_rotated>::save(ob);
+        ob << sort_result_descending;
+        ob << make_data(&output_data_type, sizeof(data_types));
+        ob << clockwise;
+        ob << selected_scores;
+        ob << valid_outputs;
+    }
+
+    void load(BinaryInputBuffer& ib) override {
+        primitive_base<nms_rotated>::load(ib);
+        ib >> sort_result_descending;
+        ib >> make_data(&output_data_type, sizeof(data_types));
+        ib >> clockwise;
+        ib >> selected_scores;
+        ib >> valid_outputs;
+    }
+};
+}  // namespace cldnn
---
+++ src/plugins/intel_gpu/src/plugin/ops/nms_rotated.cpp
@@ -0,0 +1,66 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "intel_gpu/plugin/program_builder.hpp"
+#include "intel_gpu/plugin/common_utils.hpp"
+#include "intel_gpu/primitives/nms_rotated.hpp"
+#include "intel_gpu/primitives/mutable_data.hpp"
+#include "transformations/utils/utils.hpp"
+
+#include "openvino/op/nms_rotated.hpp"
+
+namespace ov {
+namespace intel_gpu {
+
+static void CreateNMSRotatedOp(ProgramBuilder& p, const std::shared_ptr<ov::op::v13::NMSRotated>& op) {
+    validate_inputs_count(op, {5});
+    auto inputs = p.GetInputInfo(op);
+    std::string layerName = layer_type_name_ID(op);
+
+    const size_t num_outputs = op->get_output_size();
+    assert(num_outputs == 3);
+
+    std::vector<cldnn::memory::ptr> shared_memory;
+    const auto output_indices = op->get_output_partial_shape(0)[0].get_length();
+
+
+    const auto scores_precision = op->get_output_element_type(1);
+    const cldnn::layout scores_layout{
+        cldnn::element_type_to_data_type(scores_precision),
+        cldnn::format::bfyx,
+        cldnn::tensor(static_cast<int32_t>(output_indices), 3, 1, 1)};
+    shared_memory.emplace_back(p.get_engine().allocate_memory(scores_layout));
+    const cldnn::primitive_id scores_id = layer_type_name_ID(op) + "_md_write_first";
+    const cldnn::mutable_data scores_prim{scores_id, shared_memory.back()};
+    p.add_primitive(*op, scores_prim);
+    inputs.push_back(cldnn::input_info(scores_id));
+
+    auto valid_output_precision = op->get_output_element_type(2);
+    if (valid_output_precision == ov::element::i64) {
+        valid_output_precision = ov::element::i32;
+    }
+    const cldnn::layout valid_output_layout{
+        cldnn::element_type_to_data_type(valid_output_precision),
+        cldnn::format::get_default_format(op->get_output_shape(2).size()),
+        tensor_from_dims(op->get_output_shape(2))};
+    shared_memory.emplace_back(p.get_engine().allocate_memory(valid_output_layout));
+    const cldnn::primitive_id valid_output_id = layer_type_name_ID(op) + "_md_write_second";
+    const cldnn::mutable_data valid_output_prim{valid_output_id, shared_memory.back()};
+    p.add_primitive(*op, valid_output_prim);
+    inputs.push_back(cldnn::input_info(valid_output_id));
+
+    const auto prim = cldnn::nms_rotated(layerName,
+                                         inputs,
+                                         op->get_sort_result_descending(),
+                                         cldnn::element_type_to_data_type(op->get_output_type_attr()),
+                                         op->get_clockwise(),
+                                         scores_id,
+                                         valid_output_id);
+    p.add_primitive(*op, prim);
+}
+
+REGISTER_FACTORY_IMPL(v13, NMSRotated);
+
+}  // namespace intel_gpu
+}  // namespace ov
